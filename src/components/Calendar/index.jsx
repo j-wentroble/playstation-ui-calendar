38a0dc7749c5c9c6de585d6dcb5cc15c28e7c0db
j-wentroble <joshuawentroble@gmail.com>
2024-10-04 09:11:40 -0400

    add week bar and icon button

--- src/components/Calendar/index.jsx
+++ src/components/Calendar/index.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 import { startOfMonth, endOfMonth, startOfWeek, endOfWeek, eachDayOfInterval, format } from 'date-fns';
 import CalendarEvent from '../CalendarEvent';
 
+const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
 const Calendar = ({ currentMonth, events, onEventClick }) => {
   const monthStart = startOfMonth(currentMonth);
   const monthEnd = endOfMonth(currentMonth);
@@ -16,23 +18,35 @@ const Calendar = ({ currentMonth, events, onEventClick }) => {
   const numberOfRows = Math.ceil(days.length / 7);
 
   return (
-    <div className="calendar-container" style={{ gridTemplateRows: `repeat(${numberOfRows}, 1fr)` }}>
-      {days.map((day, idx) => {
-        const isCurrentMonth = format(day, 'M') === format(currentMonth, 'M'); // Check if the day is in the current month
-
-        // Filter events by matching the day with the event's date
-        const dayEvents = events.filter(event => format(new Date(event.date), 'yyyy-MM-dd') === format(day, 'yyyy-MM-dd'));
-
-        return (
-          <div key={idx} className={`day-cell ${isCurrentMonth ? '' : 'disabled'}`}>
-            {dayEvents.length > 0 ? (
-              <CalendarEvent event={dayEvents[0]} onEventClick={onEventClick} date={day} />
-            ) : (
-              <span className="date-number">{format(day, 'd')}</span>
-            )}
+    <div className="calendar-wrapper">
+      {/* Weekbar */}
+      <div className="weekbar">
+        {daysOfWeek.map((day, idx) => (
+          <div key={idx} className="weekbar-day">
+            {day}
           </div>
-        );
-      })}
+        ))}
+      </div>
+
+      {/* Calendar Grid */}
+      <div className="calendar-container" style={{ gridTemplateRows: `repeat(${numberOfRows}, 1fr)` }}>
+        {days.map((day, idx) => {
+          const isCurrentMonth = format(day, 'M') === format(currentMonth, 'M'); // Check if the day is in the current month
+
+          // Filter events by matching the day with the event's date
+          const dayEvents = events.filter(event => format(new Date(event.date), 'yyyy-MM-dd') === format(day, 'yyyy-MM-dd'));
+
+          return (
+            <div key={idx} className={`day-cell ${isCurrentMonth ? '' : 'disabled'}`}>
+              {dayEvents.length > 0 ? (
+                <CalendarEvent event={dayEvents[0]} onEventClick={onEventClick} date={day} />
+              ) : (
+                <span className="date-number">{format(day, 'd')}</span>
+              )}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
